d88becf5bda769672a138734fb3671375a959eda
Mmx <1624045573@qq.com>
2021-07-17 20:52:31 +0800

    feat:complete functions

---
+++ config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"errors"
+	"github.com/Mmx233/tool"
+	"reflect"
+)
+
+var NewConfig = errors.New("default config generated")
+
+type Options struct {
+	Config interface{}
+	Default interface{}
+	Path string
+	FillDefault bool
+}
+
+func Load(s Options)error{
+		if s.Path== "" {
+			s.Path="Config.json"
+		}
+
+	//config not exist
+		if !tool.File.Exists(s.Path) {
+			if err := tool.File.WriteJson(
+				s.Path,
+				s.Default,
+			); err != nil {
+				return err
+			}
+			return NewConfig
+		}
+
+		if err := tool.File.ReadJson(s.Path, s.Config); err != nil {
+			return err
+		}
+
+	// fill config with default value
+		if s.FillDefault {
+			Config:=reflect.ValueOf(s.Config).Elem()
+			Default:=reflect.ValueOf(s.Default).Elem()
+			for i:=0;i<Default.NumField();i++ {
+				if !Default.Field(i).IsZero() && Config.Field(i).IsZero() {
+					Config.Field(i).Set(Default.Field(i))
+				}
+			}
+		}
+
+		// fill back to ensure update
+		_ = tool.File.WriteJson(s.Path, s.Config)
+
+		return nil
+}
+
+func IsNew(e error)bool{
+	return e==NewConfig
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Mmx233/config
+
+go 1.16
+
+require (
+	github.com/Mmx233/tool v0.1.5
+	github.com/PuerkitoBio/goquery v1.7.1 // indirect
+)
