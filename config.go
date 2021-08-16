41d0f8fb90c696693ead1a0e811594c1e5c2e45f
Mmx <1624045573@qq.com>
2021-08-16 16:26:52 +0800

    feat:允许控制覆写

--- config.go
+++ config.go
@@ -9,49 +9,52 @@ import (
 var NewConfig = errors.New("default config generated")
 
 type Options struct {
-	Config interface{}
-	Default interface{}
-	Path string
+	Config      interface{}
+	Default     interface{}
+	Path        string
 	FillDefault bool
+	Overwrite   bool
 }
 
-func Load(s Options)error{
-		if s.Path== "" {
-			s.Path="Config.json"
-		}
+func Load(s Options) error {
+	if s.Path == "" {
+		s.Path = "Config.json"
+	}
 
 	//config not exist
-		if !tool.File.Exists(s.Path) {
-			if err := tool.File.WriteJson(
-				s.Path,
-				s.Default,
-			); err != nil {
-				return err
-			}
-			return NewConfig
-		}
-
-		if err := tool.File.ReadJson(s.Path, s.Config); err != nil {
+	if !tool.File.Exists(s.Path) {
+		if err := tool.File.WriteJson(
+			s.Path,
+			s.Default,
+		); err != nil {
 			return err
 		}
+		return NewConfig
+	}
+
+	if err := tool.File.ReadJson(s.Path, s.Config); err != nil {
+		return err
+	}
 
 	// fill config with default value
-		if s.FillDefault {
-			Config:=reflect.ValueOf(s.Config).Elem()
-			Default:=reflect.ValueOf(s.Default).Elem()
-			for i:=0;i<Default.NumField();i++ {
-				if !Default.Field(i).IsZero() && Config.Field(i).IsZero() {
-					Config.Field(i).Set(Default.Field(i))
-				}
+	if s.FillDefault {
+		Config := reflect.ValueOf(s.Config).Elem()
+		Default := reflect.ValueOf(s.Default).Elem()
+		for i := 0; i < Default.NumField(); i++ {
+			if !Default.Field(i).IsZero() && Config.Field(i).IsZero() {
+				Config.Field(i).Set(Default.Field(i))
 			}
 		}
+	}
 
-		// fill back to ensure update
+	// fill back to ensure update
+	if s.Overwrite {
 		_ = tool.File.WriteJson(s.Path, s.Config)
+	}
 
-		return nil
+	return nil
 }
 
-func IsNew(e error)bool{
-	return e==NewConfig
+func IsNew(e error) bool {
+	return e == NewConfig
 }
